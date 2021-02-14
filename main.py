a28e853442db8c46d5ea713ffb1c7509690366e4
chilldabrek <nik.andreev.16@mail.ru>
2021-02-14 10:26:50 +0300

    py game_cube

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
-import pygame.examples
 import pygame
-import os
-import sys
+import math
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    return image
+def hsv_rgb(gr, light):
+    hi = abs(gr / 60) // 6
+    p = light * (1 - 100)
+    q = light * (1 - (gr / 60) - abs(gr / 60) * 100)
+    t = light * (1 - (1 - ((gr / 60) - abs(gr / 60))) * 100)
+    x = 255 / 100
+    print(hi, p, q, t)
+    if hi == 0:
+        rgb = (light * x, p * x, t * x)
+    elif hi == 1:
+        rgb = (q, light, p)
+    elif hi == 2:
+        rgb = (p, light, t)
+    elif hi == 3:
+        rgb = (p, q, light)
+    elif hi == 4:
+        rgb = (t, p, light)
+    elif hi == 5:
+        rgb = (light, p, q)
+    return rgb
 
 
-size = width, heights = 400, 400
-surface = pygame.display.set_mode(size)
-sprite = pygame.sprite.Sprite()
-running = True
-all_sprites = pygame.sprite.Group()
-pygame.mouse.set_visible(False)
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if pygame.mouse.get_focused():
-            surface.fill((0, 0, 0))
-            si = pygame.mouse.get_pos()
-            sprite.image = load_image('arrow.png')
-            sprite.rect = sprite.image.get_rect()
-            sprite.rect.x, sprite.rect.y = si[0], si[1]
-            all_sprites.add(sprite)
-            all_sprites.draw(surface)
-    pygame.display.flip()
+if __name__ == '__main__':
+    # инициализация Pygame:
+    pygame.init()
+    # название
+    pygame.display.set_caption('крест')
+    # размеры окна:
+    size = width, height = 300, 300
+    # screen — холст, на котором нужно рисовать:
+    screen = pygame.display.set_mode(size)
+    w = int(input())
+    hue = int(input())
+    if w % 4 == 0 and w < 101 and 0 < hue <= 360:
+        # pygame.draw.rect(screen, (255, 255, 0), (150, 50, 100, 100))
+        start_pos = int(150 - (w / 2))
+        finish_pos = int(150 + (w / 2))
+        pos_2 = start_pos - int(w / 2)
+        print(hsv_rgb(hue, 0.75))
+        # pygame.draw.rect(screen, hsv_rgb(hue, 0.75), (start_pos, start_pos, w, w))
+        # pygame.draw.polygon(screen, hsv_rgb(hue, 1), [[150, pos_2],
+        #                                               [start_pos, start_pos], [start_pos + w, start_pos],
+        #                                               [w + (w / 2) + start_pos, pos_2]])
+        # pygame.draw.polygon(screen, hsv_rgb(hue, 0.5), [[start_pos + w, start_pos], [150 + w, pos_2], [150 + w, 150],
+        #                                                 [150 + (w / 2), 150 + (w / 2)]])
+        # # формирование кадра:
+        # # команды рисования на холсте
+        # # смена (отрисовка) кадра:
+        pygame.display.flip()
+        # # ожидание закрытия окна:
+        while pygame.event.wait().type != pygame.QUIT:
+            pass
+        # завершение работы:
+    else:
+        print('Неправильный формат ввода')
+        pygame.quit()
+    pygame.quit()
